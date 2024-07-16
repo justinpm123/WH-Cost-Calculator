85f4322a6452d224871a2aad4854cb1df815eaff
Justin Meriwether <79863533+justinpm123@users.noreply.github.com>
2024-07-16 08:38:10 -0700

    update JS to not duplicate show/hide classes

--- calc.js
+++ calc.js
@@ -1,27 +1,38 @@
-// Get the Service Type buttons
+// Get the buttons
 const digitalTransfersButton = document.querySelector('#digital_transfers_btn');
 const cadcutTransfersButton = document.querySelector('#cadcut_transfers_btn');
 
-// Get the Service sections
+// Get the sections
 const digitalTransfersSection = document.querySelector('#digital_transfers_sec');
 const cadcutTransfersSection = document.querySelector('#cadcut_transfers_sec');
 
+// Function to add a class if it doesn't exist
+function addClassIfNotExists(element, className) {
+    if (!element.classList.contains(className)) {
+        element.classList.add(className);
+    }
+}
+
 // Add click event listener for "digital_transfers_btn"
 digitalTransfersButton.addEventListener('click', () => {
-    // Remove "HIDE" class from "digital_transfers_sec"
-    digitalTransfersSection.classList.remove('HIDE');
+    // Remove "hide" class and add "show" class to "digital_transfers_sec"
+    digitalTransfersSection.classList.remove('hide');
+    addClassIfNotExists(digitalTransfersSection, 'show');
     
-    // Add "HIDE" class to "cadcut_transfers_sec"
-    cadcutTransfersSection.classList.add('HIDE');
+    // Add "hide" class and remove "show" class from "cadcut_transfers_sec"
+    cadcutTransfersSection.classList.add('hide');
+    cadcutTransfersSection.classList.remove('show');
 });
 
 // Add click event listener for "cadcut_transfers_btn"
 cadcutTransfersButton.addEventListener('click', () => {
-    // Add "HIDE" class to "digital_transfers_sec"
-    digitalTransfersSection.classList.add('HIDE');
+    // Add "hide" class and remove "show" class to "digital_transfers_sec"
+    digitalTransfersSection.classList.add('hide');
+    digitalTransfersSection.classList.remove('show');
     
-    // Remove "HIDE" class from "cadcut_transfers_sec"
-    cadcutTransfersSection.classList.remove('HIDE');
+    // Remove "hide" class and add "show" class from "cadcut_transfers_sec"
+    cadcutTransfersSection.classList.remove('hide');
+    addClassIfNotExists(cadcutTransfersSection, 'show');
 });
 // Get all buttons with class .filter-btn
 const filterButtons = document.querySelectorAll('.filter-btn');
