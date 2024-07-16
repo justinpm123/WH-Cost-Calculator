93cb68f46a4fd34534d20882ba7a4e1450047f38
Justin Meriwether <79863533+justinpm123@users.noreply.github.com>
2024-07-16 08:32:14 -0700

    Updated HTML, CSS, and JS to include HIDE SHOW classes

--- calc.js
+++ calc.js
@@ -1,3 +1,28 @@
+// Get the Service Type buttons
+const digitalTransfersButton = document.querySelector('#digital_transfers_btn');
+const cadcutTransfersButton = document.querySelector('#cadcut_transfers_btn');
+
+// Get the Service sections
+const digitalTransfersSection = document.querySelector('#digital_transfers_sec');
+const cadcutTransfersSection = document.querySelector('#cadcut_transfers_sec');
+
+// Add click event listener for "digital_transfers_btn"
+digitalTransfersButton.addEventListener('click', () => {
+    // Remove "HIDE" class from "digital_transfers_sec"
+    digitalTransfersSection.classList.remove('HIDE');
+    
+    // Add "HIDE" class to "cadcut_transfers_sec"
+    cadcutTransfersSection.classList.add('HIDE');
+});
+
+// Add click event listener for "cadcut_transfers_btn"
+cadcutTransfersButton.addEventListener('click', () => {
+    // Add "HIDE" class to "digital_transfers_sec"
+    digitalTransfersSection.classList.add('HIDE');
+    
+    // Remove "HIDE" class from "cadcut_transfers_sec"
+    cadcutTransfersSection.classList.remove('HIDE');
+});
 // Get all buttons with class .filter-btn
 const filterButtons = document.querySelectorAll('.filter-btn');
 const digiButtons = document.querySelectorAll('.digi-btn');
