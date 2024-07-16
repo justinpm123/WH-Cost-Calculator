d8d5e7e7e5f93243d962472004801fb21281f6e5
Justin Meriwether <79863533+justinpm123@users.noreply.github.com>
2024-07-16 09:46:44 -0700

    digi-btn display:none

--- calc.js
+++ calc.js
@@ -101,12 +101,12 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementsByClassName("filter-btns");
-var btns = btnContainer.getElementsByClassName("filter-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// var btnContainer = document.getElementsByClassName("filter-btns");
+// var btns = btnContainer.getElementsByClassName("filter-btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
